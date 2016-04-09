afbdc718997343ebc8355c19f404ab7c85e43adb
Denys <dfitsak@gmail.com>
2016-04-09 16:55:48 +0200

    layout to react

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -2,14 +2,14 @@ import merge from 'lodash/merge';
 import * as types from '../constants/ActionTypes';
 
 const initialFormState = {
-    user: {}
+    id: 0
 };
 
 export default function user(state = initialFormState, action) {
 
     switch(action.type) {
-        case 'GET_USER':
-            return Object.assign({}, state, action.users)
+        case 'LOGIN_USER':
+            return Object.assign({}, state, action.user)
 
         default:
             return state;
