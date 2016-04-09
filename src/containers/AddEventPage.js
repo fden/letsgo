e277041975e71ee185676214b1b40bdba7fd785c
Mikhail <miheyego@gmail.com>
2016-04-09 13:23:32 +0200

    styles for add event view

--- src/containers/AddEventPage.js
+++ src/containers/AddEventPage.js
@@ -22,9 +22,9 @@ class AddEventPage extends Component {
     handleSubmit() {
       let params = {}
 
-      params.title = this.refs.title.value
-      params.date = this.refs.date.value
-      params.description = this.refs.description.value
+      params.eventTitle = this.refs.eventTitle.value
+      params.eventDate = this.refs.eventDate.value
+      params.eventDescription = this.refs.eventDescription.value
 
       this.props.addEvent(params)
     }
@@ -33,10 +33,46 @@ class AddEventPage extends Component {
     render() {
         return (
           <div>
-            <input type="text" ref="title" />
-            <input type="text" ref="description" />
-            <input type="text" ref="date" />
-            <button onClick={this.handleSubmit.bind(this)} >add</button>
+
+
+            <div className="card mdl-card mdl-shadow--2dp">
+
+              <div className="card-header">
+                <div className="mdl-card__title card-item-bg">
+                  <h2 className="mdl-card__title-text">Add new event</h2>
+                </div>
+
+                <div className="mdl-card__supporting-text card-item-bg">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Mauris sagittis pellentesque lacus eleifend lacinia...
+                </div>
+              </div>
+
+              <div className="mdl-textfield mdl-js-textfield form-item">
+                <input className="mdl-textfield__input" type="text" ref="eventTitle" placeholder="Event title"/>
+
+              </div>
+
+              <div className="mdl-textfield mdl-js-textfield form-item">
+                <input className="mdl-textfield__input" type="text" ref="eventDate" placeholder="Event date"/>
+
+              </div>
+
+              <div className="mdl-textfield mdl-js-textfield form-item">
+                <textarea className="mdl-textfield__input" type="text" rows= "3" ref="eventDescription" placeholder="Event description"></textarea>
+
+              </div>
+
+              <div className="mdl-card__actions mdl-card--border">
+                <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                  Add event
+                </button>
+              </div>
+
+
+
+            </div>
+
           </div>
         )
     }
