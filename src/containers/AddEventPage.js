cf6fe45be9792d74e446b785a93eb376a0f98bfe
Denys <dfitsak@gmail.com>
2016-04-09 13:21:10 +0200

    add map

--- src/containers/AddEventPage.js
+++ src/containers/AddEventPage.js
@@ -2,12 +2,16 @@ import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { addEvent } from '../actions/eventActions'
 import zip from 'lodash/zip'
-
+import MapAddMarker from '../components/map'
 
 
 class AddEventPage extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+          lat: 0,
+          lon: 0
+        }
         //require("../styles/less/ppvDashboard.less");
     }
 
@@ -21,18 +25,26 @@ class AddEventPage extends Component {
 
     handleSubmit() {
       let params = {}
-
       params.title = this.refs.title.value
       params.date = this.refs.date.value
       params.description = this.refs.description.value
-
+      params.lat = this.state.lat
+      params.lon = this.state.lon
       this.props.addEvent(params)
+    }
+
+    handleChangePosition(lat, lon) {
+        this.setState({
+          lat:lat,
+          lon: lon
+        }
     }
 
 
     render() {
         return (
           <div>
+            <MapAddMarker onChange={this.handleChangePosition.bind(this)}/>
             <input type="text" ref="title" />
             <input type="text" ref="description" />
             <input type="text" ref="date" />
