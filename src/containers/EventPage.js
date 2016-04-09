6ba27db45f2d6cd35f840b2466f74a09fdf5c949
Mikhail <miheyego@gmail.com>
2016-04-09 16:31:10 +0200

    adding attenders

--- src/containers/EventPage.js
+++ src/containers/EventPage.js
@@ -68,6 +68,17 @@ class EventPage extends Component {
               <div>
                 <ShowMap lat={lat} lng={lon}/>
               </div>
+              <div className="attenders">
+                  <h5>Attenders: 5</h5>
+              </div>
+              <div className="attenders-pics">
+                <img src="https://randomuser.me/api/portraits/men/1.jpg"/>
+                <img src="https://randomuser.me/api/portraits/men/2.jpg" />
+                <img src="https://randomuser.me/api/portraits/men/3.jpg" />
+                <img src="https://randomuser.me/api/portraits/men/5.jpg" />
+                <img src="https://randomuser.me/api/portraits/men/7.jpg" />
+
+              </div>
             </div>
             <div className="mdl-cell mdl-cell--8-col content">
               <h3>{title}</h3>
